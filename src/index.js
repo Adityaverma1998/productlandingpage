346831a23880c027f1cb97cc30d0a9695ab0b79f
Aditya <vermaaditya860@gmail.com>
2023-01-31 23:03:15 +0530

    initial project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Master from './Master';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Master />
+  </React.StrictMode>
+);
