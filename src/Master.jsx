a4a707c58f1b6d8037fde9b9b0384ea561cead74
Aditya <vermaaditya860@gmail.com>
2023-02-07 23:28:25 +0530

    social

--- src/Master.jsx
+++ src/Master.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from './component/HeroSection'
 import Pricing from './component/Pricing'
+import Socialproof from './component/Socialproof'
 
 const Master = () => {
   return (
     <>
     <Pricing/>
     <HeroSection/>
+    <Socialproof/>
     </>
   )
 }
